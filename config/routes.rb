9baf4120641bc3d96685563d559965af1b7e38c3
Vashira <vashimgs@gmail.com>
2020-06-03 00:40:33 +0100

    updated the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'recipes/index'
+  root 'recipes#index'
+  get 'recipes/index' => 'recipes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
