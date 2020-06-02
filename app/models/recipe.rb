59d25dbad6b3570c315208c7e5086df4bb06e3c9
Vashira <vashimgs@gmail.com>
2020-06-03 00:33:40 +0100

    Added recipes index method, mock_http file and  session store file

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,12 @@
 class Recipe < ApplicationRecord
+  include HTTParty
+        
+  base_uri 'http://www.recipepuppy.com/api'
+  default_params onlyImages: 1
+
+  format :json
+
+  def self.for ( keyword = 'chocolate' )
+    get('', query: { q: keyword })['results']
+  end
 end
---
+++ config/initializers/mock_http_requests.rb
@@ -0,0 +1,12 @@
+if Rails.configuration.use_mocks_please
+  require 'webmock'
+  include WebMock::API
+
+  WebMock.enable!
+
+  stub_request(:get, /recipepuppy\.com\/api\//).
+      to_return { |request|
+          matched_result = request.uri.query.match(/q=(.*)/)
+          { body: File.new("#{Rails.root}/spec/mocks/#{matched_result[1]}.json"), status: 200 }
+      }             
+end
